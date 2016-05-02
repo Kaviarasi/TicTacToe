f51f0f10f199d02e6c8e23eb866f45fde0bde225
Kaviarasi <kavivenkat04@gmail.com>
2016-05-02 14:27:05 +0530

    First commit

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(65, 80);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(63, 55);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(134, 80);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(63, 55);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(203, 80);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(63, 55);
+            this.button3.TabIndex = 2;
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(65, 143);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button4.Name = "button4";
+            this.button4.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button4.Size = new System.Drawing.Size(63, 55);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(136, 143);
+            this.button5.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(63, 55);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.Location = new System.Drawing.Point(205, 143);
+            this.button6.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(63, 55);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.Location = new System.Drawing.Point(65, 206);
+            this.button7.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(63, 55);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.Location = new System.Drawing.Point(136, 206);
+            this.button8.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(63, 55);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.Font = new System.Drawing.Font("Kalinga", 20F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.Location = new System.Drawing.Point(205, 206);
+            this.button9.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(63, 55);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Goudy Stout", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox1.Location = new System.Drawing.Point(65, 34);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(190, 22);
+            this.textBox1.TabIndex = 9;
+            this.textBox1.Text = " IT\'S  X O";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.Menu;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Engravers MT", 10F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.textBox2.Location = new System.Drawing.Point(66, 289);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(202, 16);
+            this.textBox2.TabIndex = 10;
+            this.textBox2.Text = "!!!! PLAY !!!!";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button10
+            // 
+            this.button10.Location = new System.Drawing.Point(12, 323);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(110, 32);
+            this.button10.TabIndex = 11;
+            this.button10.Text = "NEW GAME";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Location = new System.Drawing.Point(203, 323);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(110, 32);
+            this.button11.TabIndex = 12;
+            this.button11.Text = "CLOSE GAME";
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
+            this.ClientSize = new System.Drawing.Size(331, 363);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Font = new System.Drawing.Font("Kalinga", 8.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "XOXOXO";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        /* to maintain the turn. toggles after every move */
+        public int flg;
+        /* atmost 9 moves are feasible. */
+        public int cnt = 0;
+        /* to check whether game is over */
+        public bool over = false;
+        /* to check the respective cell is already filled by any player*/
+        public bool b1 = true, b2 = true, b3 = true, b4 = true, b5 = true, b6 = true, b7 = true, b8 = true, b9 = true;
+        public Form1()
+        {
+            flg = 0; /* X will start the game*/
+            InitializeComponent();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (b5 && !over && cnt<9)
+            {
+                cnt++;
+                if (flg == 0)
+                    button5.Text = "X";
+                else
+                    button5.Text = "O";
+                flg = ~flg;
+                if ((button1.Text == button5.Text && button5.Text == button9.Text) ||
+    (button3.Text == button5.Text && button5.Text == button7.Text) ||
+    (button4.Text == button5.Text && button5.Text == button6.Text) ||
+                (button2.Text == button5.Text && button5.Text == button8.Text))
+                {
+                    over = true;
+                    if (button5.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b5 = false;
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (b1 && !over && cnt<9)
+            {
+                cnt++;
+                if (flg == 0)
+                    button1.Text = "X";
+                else
+                    button1.Text = "O";
+                flg = ~flg;
+                if ((button1.Text == button2.Text && button1.Text == button3.Text) ||
+                    (button1.Text == button4.Text && button4.Text == button7.Text) ||
+                    (button1.Text == button5.Text && button5.Text == button9.Text))
+                {
+                    over = true;
+                    if (button1.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b1 = false;
+            }
+
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void progressBar1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (b2 && !over && cnt<9)
+            {
+                cnt++;
+                if (flg == 0)
+                    button2.Text = "X";
+                else
+                    button2.Text = "O";
+                flg = ~flg;
+                if ((button1.Text == button2.Text && button2.Text == button3.Text) 
+                    || (button5.Text == button2.Text && button2.Text == button8.Text))
+                {
+                    over = true;
+                    if (button2.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b2 = false;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (b3 && !over && cnt < 9)
+            {
+                cnt++;
+                if (flg == 0)
+                    button3.Text = "X";
+                else
+                    button3.Text = "O";
+                flg = ~flg;
+                if ((button1.Text == button2.Text && button1.Text == button3.Text) ||
+        (button3.Text == button6.Text && button6.Text == button9.Text) ||
+        (button3.Text == button5.Text && button5.Text == button7.Text))
+                {
+                    over = true;
+                    if (button3.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b3 = false;
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (b4 && !over && cnt<9)
+            {
+                cnt++;
+
+                if (flg == 0)
+                    button4.Text = "X";
+                else
+                    button4.Text = "O";
+                flg = ~flg;
+                if ((button5.Text == button4.Text && button4.Text == button6.Text) || (button1.Text == button4.Text && button4.Text == button7.Text))
+                {
+                    over = true;
+                    if (button4.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b4 = false;
+            }
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (b6 && !over && cnt < 9)
+            {
+                cnt++;
+                if (flg == 0)
+                    button6.Text = "X";
+                else
+                    button6.Text = "O";
+                flg = ~flg;
+                if ((button4.Text == button6.Text && button6.Text == button5.Text) || (button3.Text == button6.Text && button6.Text == button9.Text))
+                {
+                    over = true;
+                    if (button6.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b6 = false;
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            if (b9 && !over && cnt < 9)
+            {
+                cnt++;
+                if (flg == 0)
+                    button9.Text = "X";
+                else
+                    button9.Text = "O";
+                flg = ~flg;
+                if ((button9.Text == button6.Text && button6.Text == button3.Text) ||
+        (button7.Text == button8.Text && button8.Text == button9.Text) ||
+        (button1.Text == button5.Text && button5.Text == button9.Text))
+                {
+                    over = true;
+                    if (button9.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b9 = false;
+            }
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            if (b8 && !over && cnt < 9)
+            {
+                cnt++;
+                if (flg == 0)
+                    button8.Text = "X";
+                else
+                    button8.Text = "O";
+                flg = ~flg;
+                if ((button6.Text == button7.Text && button7.Text == button8.Text) || (button5.Text == button2.Text && button2.Text == button8.Text))
+                {
+                    over = true;
+                    if (button7.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b8 = false;
+            }
+        }
+        private void button7_Click(object sender, EventArgs e)
+        {
+            if (b7 && !over && cnt < 9)
+            {
+                cnt++;
+                if (flg == 0)
+                    button7.Text = "X";
+                else
+                    button7.Text = "O";
+                flg = ~flg;
+                if ((button7.Text == button8.Text && button8.Text == button9.Text) || 
+                    (button1.Text == button7.Text && button7.Text == button4.Text) ||
+                    (button5.Text == button7.Text && button7.Text == button3.Text))
+                {
+                    over = true;
+                    if (button7.Text == "O")
+                        textBox2.Text = "Player 2 Wins!!";
+                    else
+                        textBox2.Text = "Player 1 Wins!!";
+                }
+                b7 = false;
+            }
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            while (Controls.Count > 0)
+            {
+                Controls[0].Dispose();
+            }
+        cnt = 0;
+        over = false;
+        b1 = true; b2 = true; 
+            b3 = true; b4 = true;
+            b5 = true; b6 = true; b7 = true; b8 = true; b9 = true;
+            flg = 0;
+        
+            InitializeComponent();
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
